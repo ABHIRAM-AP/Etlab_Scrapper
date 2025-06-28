bf1332a2a95bcdcf082de3164073c8325ab32c08
Abhiram <abhiramap05@gmail.com>
2025-06-28 18:29:59 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from web import fetch_data
+import os
 
 app = Flask(__name__)
 
@@ -13,4 +14,6 @@ def get_data():
 
 
 if __name__ == "__main__":
-     app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
+
