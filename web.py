0f7acf8d0d7d727c338f83b6391cc157b1a3c01a
Abhiram <abhiramap05@gmail.com>
2025-07-12 20:46:04 +0530

    added fetch_subjects

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from flask import jsonify
 
@@ -11,7 +12,7 @@ headers = {
 
 def fetch_data(userID, password):
     session = requests.Session()
-
+    
     login_data = {
         "LoginForm[username]": userID,
         "LoginForm[password]": password,
@@ -26,7 +27,7 @@ def fetch_data(userID, password):
         "title": [],
         "data": [],
     }
-
+    subject_data = fetch_subjects(session)
     attendance_response = session.get(attendance_url, headers=headers)
     if attendance_response.status_code == 200:
         attend_soup = BeautifulSoup(attendance_response.text, "html.parser")
@@ -38,10 +39,44 @@ def fetch_data(userID, password):
                 if cols:
                     attendance_data["data"] = cols
 
+
+    
     #  Final JSON 
     return jsonify({
         "attendance": attendance_data,
+        "subjects":subject_data,
     })
 
 
+def fetch_subjects(session):
+    subject_url =  "https://sctce.etlab.in/ktuacademics/student/teacher"
+    response = session.get(subject_url, headers=headers)
 
+    if response.status_code != 200:
+        return {"error": "Failed to fetch subjects"}
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    tables = soup.find_all("table")
+    if not tables:
+        return {"error": "No tables found"}
+
+    last_table = tables[-1]
+    t_body = last_table.find("tbody")
+    rows = t_body.find_all("tr")
+
+    subjects_dict = {}
+    for row in rows:
+        tds = row.find_all("td")
+        if len(tds) >= 3:
+            second_td = tds[2].get_text(strip=True)
+            regex_match = re.match(r"([A-Z]{3}\d{3})\s*-\s*(.+)", second_td)
+            if regex_match:
+                sub_code = regex_match.group(1).strip()
+                sub_name = regex_match.group(2).strip().upper()
+
+                if sub_code not in subjects_dict:
+                    subjects_dict[sub_code] =sub_name
+                
+
+    print(subjects_dict)
+    return {"subjects": subjects_dict}
